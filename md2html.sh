968e30e751ed924da22560c158e1ae7cac7c5ae0
queerkid <queerki@qq.com>
2016-10-26 21:53:42 +0800

    lalala

--- md2html.sh
+++ md2html.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
-articlelist=''
+
+cat ./index_first.html > index.html
 
 for md in ./articles/markdown/*.md; do
     arr=(${md//// })
     fn=(${arr[3]//.md/}) #file name
     pandoc -s "$md" -o ./articles/html/${fn}.html  -H header.html
     fl=$(head -1 ${md}) #file first line  
-    node='\n<p><a href="articles/html/'${fn}.html'">'${fl:1}'</a></p>' #fl:1 is tile
-    articlelist+=$node
+    node='<p><a href="articles/html/'${fn}.html'">'${fl:1}'</a></p>' #fl:1 is tile 
+    echo "          "$node >> index.html  
 done
 
-#splicing file
-cat ./index_first.html > index.html
-echo $articlelist >> index.html 
 cat ./index_last.html >> index.html
 
 # go github and look my blog
